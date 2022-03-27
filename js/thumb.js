a786c98615acaae130459a806356bc3ec5784a22
radamid <dimasjw@bk.ru>
2022-03-27 11:09:11 +0500

    Рефакторинг полноэкранного показа изображения

--- js/thumb.js
+++ js/thumb.js
@@ -12,11 +12,13 @@ const template = templateFragment.querySelector('.picture');
 
 const fragment = document.createDocumentFragment();
 
-photosData.forEach(({url, description, comments, likes}) => {
+photosData.forEach(({id, url, description, comments, likes}) => {
   const element = template.cloneNode(true);
 
   element.querySelector('.picture__img').src = url;
   element.querySelector('.picture__img').alt = description;
+  // element.querySelector('.picture__img').classList.add(`picture__img--${ id }`);
+  element.querySelector('.picture__img').id = id;
   element.querySelector('.picture__comments').textContent = comments.length;
   element.querySelector('.picture__likes').textContent = likes;
 
