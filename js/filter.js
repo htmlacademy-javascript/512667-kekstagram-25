a7cbc9dc92bec730f1d7af35312f5359d7e7c0df
radamid <dimasjw@bk.ru>
2022-03-30 20:29:23 +0500

    Реализует переключение активной кнопки в фильтре для сортировки фото

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,21 @@
 const filter = document.querySelector('.img-filters');
+const filtersForm = filter.querySelector('.img-filters__form');
+const filtersButton = filtersForm.querySelectorAll('.img-filters__button');
+
+const showActiveButton = (evt) => {
+  evt.preventDefault();
+
+  if (!evt.target.className.match('img-filters__button--active') && evt.target.className.match('img-filters__button')) {
+    for (let i = 0; i < filtersButton.length; i++) {
+      filtersButton[i].classList.remove('img-filters__button--active');
+    }
+    evt.target.classList.add('img-filters__button--active');
+  }
+};
 
 const showFilterMenu = () => {
   filter.classList.remove('img-filters--inactive');
+  filtersForm.addEventListener('click', showActiveButton);
 };
 
 export {
